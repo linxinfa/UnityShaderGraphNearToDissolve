59cc0684d59fb0179d599d201e1ad07864f4962f
linxinfa <37791251+linxinfa@users.noreply.github.com>
2021-01-13 13:44:50 +0800

    Unity ShaderGraph Near to Dissolve Effect

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public Material mat;
+    public Transform m_targetObj;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        mat.SetVector("Vector3_B45F07CF", m_targetObj.position);
+    }
+}
